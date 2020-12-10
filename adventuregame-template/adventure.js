ca3f14e2741e2ec5a4fc41ef24b5ec37c80a5174
99062751 <70890704+99062751@users.noreply.github.com>
2020-12-10 09:12:11 +0100

    update

--- adventuregame-template/adventure.js
+++ adventuregame-template/adventure.js
@@ -1,26 +1,94 @@
-var item= document.getElementById("inventoryItem");
-var image= document.getElementById("image");
-var option_one= document.getElementById("button1");
-var option_two= document.getElementById("button2");
-var option_three= document.getElementById("button3");
-var start= false;
-var source = new EventSource("http://127.0.0.1:5500/adventure.html");
+var items = {item:document.getElementById("inventoryItem"), image:document.getElementById("game-container"), option_one: document.getElementById("button1"), 
+option_two: document.getElementById("button2"), option_three: document.getElementById("button3")}; 
 
-source.onopen = start= true;
-    if(start == true){
-        item.style.display= "none";
-        document.getElementById("image").style.backgroundImage= "url('img/ ' + 'house + '.jpg')";
-        document.getElementById("description").innerHTML="Level 1";
-        document.getElementById("text").innerHTML= "Je bent verdwaald tijdens Halloween en je komt dit huis tegen." 
-        + "<br>"+ "Wat ga je doen?";
-        document.getElementById("button1").innerHTML= "Aanbellen";
-        document.getElementById("button2").innerHTML= "WEGRENNEN!!!";
-        option_three.style.display= "none";
-    }
-    if(option_two.addEventListener("click", ko)){
-        function ko(){
-            document.getElementById("text").innerHTML= "Je rent weg! Na een tijdje begin je weer te lopen en kom je weer terug bij het huis! Wat ga je doen?";
-            document.getElementById("button1").innerHTML= "Aanbellen";
-            option_two.style.display= "none";
-        }
-    }
+var image1 = document.getElementById("game-container");
+intro();
+function intro(){
+    items['option_one'].onclick = function(){start()};
+    items['option_two'].onclick = function(){myScript};
+    items['option_three'].onclick = function(){myScript};
+    document.getElementById("title").innerHTML="HAUNTED: THE GAME";
+    items['item'].style.display= "none";
+    document.getElementById("description").innerHTML="BEGINNING OF GAME"+
+    "<p>Welkom, bij HAUNTED: THE GAME!" 
+    + "<br>" + "In deze game krijg je verschillende vragen met keuzes die je moet maken." 
+    + "<br>" + "Die zien eruit als deze knoppen, laten we beginnen!</p>"; 
+
+    document.getElementById("button1").innerHTML= "START";
+    document.getElementById("button2").style.display= "none";
+    document.getElementById("button3").style.display= "none";
+}
+function start(){
+    items['item'].style.display= "none";
+    items['image'].style.backgroundImage = 'url("img/house.jpg")';
+    document.getElementById("description").innerHTML="Level 1";
+    document.getElementById("description").innerHTML= "Je bent verdwaald tijdens Halloween en je komt dit huis tegen." 
+    + "<br>"+ "Wat ga je doen?";
+    document.getElementById("button1").innerHTML= "AANBELLEN";
+    document.getElementById("button2").innerHTML= "WEGRENNEN!!!";
+    document.getElementById("button2").style.display= "inline";
+    items['option_three'].style.display= "none";
+    items['option_one'].onclick = function(){route1()};
+    items['option_two'].onclick = function(){route2()};
+
+}      
+function route1(){              //Aanbellen
+    items['image'].style.backgroundImage = 'url("img/kerker.jpg")';
+    items['image'].style.left = 0 + "px";
+
+    items['item'].src = "img/key.png";
+    items['item'].style.display= "block";
+
+    document.getElementById("description").innerHTML= "Je belt aan, en hoort een rare gil! Dan is alles zwart.." 
+    + "<br>"+ "Je word wakker in een kooi die in een soort kelder ligt, je moet ontsnappen. Je hoort dat gegil weer! Wat ga je doen?";
+    document.getElementById("button1").innerHTML= "Kijken of je iets in je zakken hebt.";
+    document.getElementById("button2").innerHTML= "Kooi openmaken!";
+
+    items['item'].style.display= "none";
+    items['option_three'].style.display= "none";
+    items['option_two'].style.display= "inline";
+    items['option_one'].onclick = function(){pocket()};
+    items['option_two'].onclick = function(){open()};
+}
+function route2(){              //Wegrennen
+    document.getElementById("description").innerHTML= "Je rent weg! Na een tijdje begin je weer te lopen en kom je weer terug bij het huis! Wat ga je doen?";
+    document.getElementById("button1").innerHTML= "Aanbellen";
+    items['option_three'].style.display= "none";
+    items['option_two'].style.display= "none";
+}
+
+function pocket(){              //Kijken of je wat in je zakken hebt
+    document.getElementById("description").innerHTML= "Je hebt niks in je zakken! Gestrest kijk je weer terug wie er benenden is gekomen, en je staat oog in oog met een MONSTER!"
+        + "<br>" + "Je word leven opgegeten en gaat dood.";
+    document.getElementById("button1").innerHTML= "Opnieuw Beginnen";
+    items['option_three'].style.display= "none";
+    items['option_two'].style.display= "none";
+    items['option_one'].onclick = function(){intro()};
+}
+
+function open(){              //Kooi openmaken!
+    items['item'].style.display= "block";
+    items['item'].style.position= "absolute";
+    items['item'].style.bottom= "50px";
+    items['item'].style.right= "80px";
+    items['item'].style.cursor= "pointer";
+    document.getElementById("description").innerHTML= "Je hebt geen SLEUTEL zoek hem snel!";
+    items['option_one'].style.display= "none";
+    items['option_two'].style.display= "none";
+    items['option_three'].style.display= "none";
+
+    items['item'].onclick= function(){
+        document.getElementById("description").innerHTML= "Mooi, je hebt hem gevonden maak snel de kooi open!";
+        items['option_three'].style.display= "none";
+        items['option_two'].style.display= "inline";
+        items['option_one'].style.display= "none";
+        document.getElementById("button1").innerHTML= "Kooi openmaken";
+        document.getElementById("button1").onclick = function(){open_cage()};
+    };
+}
+
+function open_cage(){
+    document.getElementById("description").innerHTML= "Mooi, je bent eruit. Je hoort dat er NU iets naar beneden komt! Zoek snel uit hoe je hier wegkomt!";
+}
+
+
