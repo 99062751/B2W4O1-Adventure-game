09b9a7e69f3513f9d38b7d6a074593ddb0565fd2
99062751 <70890704+99062751@users.noreply.github.com>
2020-12-11 11:59:23 +0100

    update

--- adventuregame-template/adventure.js
+++ adventuregame-template/adventure.js
@@ -1,7 +1,7 @@
 var items = {item:document.getElementById("inventoryItem"), image:document.getElementById("game-container"), option_one: document.getElementById("button1"), 
 option_two: document.getElementById("button2"), option_three: document.getElementById("button3")}; 
 
-var oppakken= {sleutel:false};
+var oppakken= {sleutel:false, wapen:false};
 
 var image1 = document.getElementById("game-container");
 intro();
@@ -62,9 +62,18 @@ function route2(){             //Wegrennen
     items['option_two'].style.display= "none";
 }
 function pocket(){             //Kijken of je wat in je zakken hebt
-    document.getElementById("description").innerHTML= "Je hebt niks in je zakken! Gestrest kijk je weer terug wie er benenden is gekomen, en je staat oog in oog met een MONSTER!"
+    document.getElementById("description").innerHTML= "Je hebt niks in je zakken! Gestrest kijk je weer terug wie er benenden is gekomen, en je staat oog in oog met het monster!"
         + "<br>" + "Je word leven opgegeten en gaat dood.";
     document.getElementById("button1").innerHTML= "Opnieuw Beginnen";
+    items['item'].src = "img/monster.png";
+    items['item'].style.display = "block";
+    items['item'].style.transform = "rotate(0deg)";
+    items['item'].style.msTransform= "rotate(0deg)";
+    items['item'].style.top= "350px";
+    items['item'].style.right= "400px";
+    items['item'].style.height= "500px";
+    items['item'].style.opacity= "1";
+    items['item'].style.width= "25%";
     items['option_three'].style.display= "none";
     items['option_two'].style.display= "none";
     items['option_one'].onclick = function(){intro()};
@@ -84,6 +93,7 @@ function open(){              //Kan niet openmaken
     //sleutel
     items['item'].style.display= "block";
     items['item'].onclick= function(){
+        alert("Je hebt een sleutel gevonden!");
         oppakken["sleutel"]= true;
         items['item'].src = "img/vent.png";
         clicked();
@@ -117,10 +127,10 @@ function ventopen(){          //Rooster openen
         items['item'].style.width = "36%";
         items['item'].style.height = "34%";
         items['item'].style.transform = "rotate(55deg)";
-    items['item'].style.msTransform= "rotate(100deg)";
-    items['item'].style.right = "245px";
-    items['item'].style.top = "55px";
-    items['item'].onclick = function(){ventleft();};
+        items['item'].style.msTransform= "rotate(100deg)";
+        items['item'].style.right = "245px";
+        items['item'].style.top = "55px";
+        items['item'].onclick = function(){ventleft();};
     }else{
         document.getElementById("description").innerHTML= "Oeps! Je hebt geen sleutel. Je word opgegeten door het monster wat naar beneden is gekomen."
     }   
@@ -139,19 +149,37 @@ function ventleft(){          //als je in het huis bent
     items['option_three'].innerHTML = "Rechtertrap";
     items['option_two'].style.marginLeft = "100px";
     items['option_two'].style.marginRight = "100px";
-    items['option_one'].onclick= function(){raam()};
+    items['option_one'].onclick= function(){hallwayLeft()};
     items['option_two'].onclick= function(){monster()};
-    items['option_three'].onclick= function(){DeadEnd()};
+    items['option_three'].onclick= function(){hallwayRight()};
 }
+
+function hallwayLeft(){
+    document.getElementById("description").innerHTML= "Wat doe je hier? Loop maar verder.";
+    items['image'].style.backgroundImage = 'url("img/hall2.jpg")';
+    items['option_one'].innerHTML= "Verder lopen";
+    items['option_one'].onclick= function(){raam()};
+    items['option_two'].display= "none";
+    items['option_three'].display= "none";
+}
+function hallwayRight(){
+    document.getElementById("description").innerHTML= "Wat doe je hier? Loop maar verder.";
+    items['image'].style.backgroundImage = 'url("img/hallway.jpg")';
+    items['option_three'].innerHTML= "Verder lopen";
+    items['option_three'].onclick= function(){weapon()};
+    items['option_two'].style.display= "none";
+    items['option_one'].style.display= "none";
+}
+
 function raam(){              //Als je bij het raam bent
     items['image'].style.backgroundImage = 'url("img/window.png")';
-    items['image'].style.width= "500px"
     document.getElementById("description").innerHTML= "Je bent bij een raam. Opeens hoor je iets ook naar boven komen!" + "<br>" + 
     "Wat ga je doen?";
     items['option_one'].innerHTML = "Spring door raam";
     items['option_one'].onclick= function(){DeadEnd()};
     items['option_two'].innerHTML = "Deur dicht doen";
     items['option_three'].innerHTML = "Deur dicht doen";
+    document.getElementById("description").innerHTML= ""
 }
 
 function DeadEnd(){           //Vanzelfsprekend
@@ -167,9 +195,64 @@ function monster(){         //Als je de sporen volgt
     items['item'].style.msTransform= "rotate(0deg)";
     items['item'].style.top= "400px";
     items['item'].style.height= "500px";
+    items['item'].style.height= "500px";
+    items['item'].style.opacity= "1";
+    items['item'].style.width= "25%";
+
     items['option_one'].innerHTML = "Vechten";
     items['option_two'].innerHTML = "Rennen";
     items['option_three'].style.display = "none";
     document.getElementById("description").innerHTML= "Je volgt de sporen en na een tijdje houden de sporen op en kijk je recht"
     + "<br>" + "in de ogen van het monster! Wat ga je doen?!";
+
+    items['option_one'].onclick = function(){fight()};
+    items['option_two'].onclick = function(){run()};
+}
+function weapon(){          //Als je rechts gaat
+    document.getElementById("description").innerHTML= "Je loopt de trap op en komt in deze kamer. Wat ga je doen?";
+    items['image'].style.backgroundImage = 'url("img/bedroom.jpg")';
+    items['option_one'].innerHTML= "Terug";
+    items['option_one'].style.display= "inline";
+    items['option_two'].style.display= "none";
+    items['option_three'].style.display= "none";
+
+    items['item'].src= "img/weapon.png";
+    items['item'].style.display= "block";
+    items['item'].style.width= "80px";
+    items['item'].style.height= "240px";
+    items['item'].style.transform = "rotate(-20deg)";
+    items['item'].style.msTransform= "rotate(-20deg)";
+    items['item'].style.top= "400px";
+    items['item'].style.left= "500px";
+    items['item'].style.opacity= "0.5";
+    items['item'].onclick= function(){
+    alert("Je hebt een wapen gevonden!");
+    oppakken['wapen'] = true;
+    items['item'].style.display= "none";
+    };
+    if(oppakken['wapen'] = true){
+        items['option_one'].onclick= function(){right_back()};
+    }else if(oppakken['wapen'] == false){
+        items['option_one'].onclick= function(){DeadEnd2()};  
+    }
+}   
+function right_back(){            //Als je van rechts terug gaat
+    document.getElementById("description").innerHTML= "Je bent weer terug waar je was. Wat ga je doen?";
+    items['image'].style.backgroundImage = 'url("img/hallway.jpg")';
+    items['option_one'].onclick= function(){ventleft()
+    document.getElementById("description").innerHTML= "Je bent weer terug waar je was. Wat ga je doen?";
+    }; 
+}  
+function DeadEnd2(){           //Als je van rechts terug gaat zonder wapen
+    document.getElementById("description").innerHTML= "Je gaat de gang uit, komt het monster tegen en gaat dood.";
+    items['item'].src = "img/monster.png";
+    items['item'].style.display = "block";
+    items['item'].style.transform = "rotate(0deg)";
+    items['item'].style.msTransform= "rotate(0deg)";
+    items['item'].style.top= "350px";
+    items['item'].style.right= "400px";
+    items['item'].style.height= "500px";
+    items['item'].style.opacity= "1";
+    items['item'].style.width= "25%";
+    items['image'].style.backgroundImage = 'url("img/hallway.jpg")';
 }
